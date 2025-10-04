3ae2d00c132f4b6162b932dfbe643b6846c5bb1e
65160116 <65160116@go.buu.ac.th>
2025-10-05 05:12:18 +0700

    เเก้ปัญหากรองบิลโต๊ะเฉพาะคนที่เเสกนโ๖ีะนี้วันนี้

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -14,9 +14,19 @@ export async function findLatestContactByTableNo(tableNo: number): Promise<Conta
     JOIN tableqr t ON t.TableID = s.TableID
     JOIN customer c ON c.CustomerID = s.CustomerID
     WHERE t.TableID = ?
+    AND s.ScanTime >= CURRENT_DATE()
+    AND s.ScanTime <  CURRENT_DATE() + INTERVAL 1 DAY
     ORDER BY s.ScanTime DESC
-    LIMIT 1 
+    LIMIT 5 
   `; // ^^เเสดงรายชื่อคนในโต๊ะที่เเสกน หน้าข้อมูลติดต่อ LIMIT 1
+
+ /* 
+ ตั้งแต่เวลา date_order ของโต๊ะนั้น → ถึงเวลา “ปิดรอบ” (เช่น 23:30) เปลี่ยนได้
+  WHERE t.TableID = :tableNo
+  AND s.ScanTime >= :start
+  AND s.ScanTime <  :end
+  ORDER BY s.ScanTime DESC
+ */
   try {
     const [rows] = await pool.query(sql, [tableNo]);
     const r: any = (Array.isArray(rows) && rows.length) ? rows[0] : null;
